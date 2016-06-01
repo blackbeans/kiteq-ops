49587e3212b7534ca633945f7ce97b3c47c4706d
blackbeans <blackbeans.zc@gmail.com>
2016-06-01 16:42:02 +0800

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cp -rf vendor/ ../ 
+cp -rf vendor/* ../ 
 
 
 
 go get github.com/revel/cmd/revel
-go get github.com/revel/revel
-go get github.com/revel/modules/jobs/app/jobs
-go get github.com/blackbeans/log4go
-go get github.com/blackbeans/go-zookeeper/zk
-go get gopkg.in/mgo.v2
-go get github.com/robfig/cron
-go github.com/revel/modules/static/app/controllers
-go golang.org/x/net/websocket
+# go get github.com/revel/revel
+# go get github.com/revel/modules/jobs/app/jobs
+# go get github.com/blackbeans/log4go
+# go get github.com/blackbeans/go-zookeeper/zk
+# go get gopkg.in/mgo.v2
+# go get github.com/robfig/cron
+# go get github.com/revel/modules/static/app/controllers
+# go get golang.org/x/net/websocket
 
 
 
